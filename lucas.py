d2a2f39e75bdcc843e5d911fa096642bcd76d987
lusquinha25 <70208395+lusquinha25@users.noreply.github.com>
2020-08-25 11:19:59 -0300

    Update lucas.py

--- lucas.py
+++ lucas.py
@@ -1,2 +1,14 @@
-print('oi')
+def imc(cliente):
+    p = cliente['peso']
+    a = cliente['altura']
+    imc = p / (a * a)
+    return imc
 
+pessoa = {
+    'nome': 'Lucas Alexandre Virginio',
+    'idade': 18,
+    'peso': 67,
+    'altura': 1.72
+}
+
+print(imc(pessoa))
